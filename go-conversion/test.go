fd97fa5bdd81cea42558c049fcf90bc8dbedabf0
eghysut <egiajjah15@gmail.com>
2023-08-17 20:41:54 +0700

    fix: Add conversion test.go file

--- go-conversion/test.go
+++ go-conversion/test.go
@@ -1,1 +1,25 @@
+package main
 
+import (
+    "fmt"
+)
+
+func main() {
+
+    // konversi int8 ke int32
+    var nilai8 int8 = 127 
+    var nilai32 int32 = int32(nilai8)
+    fmt.Printf("%d bertipe %T\n", nilai32, nilai32)
+
+    // konversi nilai int 144 ke alphabet
+    var nilai int64 = 114
+    var nama string = string(nilai)
+    fmt.Printf("nilai: %d alphabet: %s\n", nilai, nama)
+    
+    // konversi byte ke alphabet
+    fname := "Hello World"
+    indeks := string(fname[6])
+    fmt.Println(fname)
+    fmt.Println("Indeks ke 6 adalah:", indeks)
+
+}
