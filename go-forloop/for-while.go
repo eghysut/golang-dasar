7476657e4afc0b20c23d0665676d9e00529de3b0
eghysut <egiajjah15@gmail.com>
2023-08-21 12:07:28 +0700

    fix: Add for-while file

---
+++ go-forloop/for-while.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+// For Loop Sebagai While Loop
+// inisialisasi := 1
+//for inisialisasi <= 5 {
+//    fmt.Println(inisialisasi)
+//    inisialisasi++
+//}
+
+func main() {
+    
+    init := 1
+    for init <= 3 {
+	fmt.Println("for:", init)
+	init++
+    }
+    fmt.Println("end:", init)
+    // Output:
+    //for: 1
+    //for: 2
+    //for: 3
+    //end: 4
+
+    i := init
+    for i <= 10 {
+	i++
+	fmt.Println("for:", i)
+    }
+    fmt.Println("end:", i)
+    // Output:
+    // for: 5
+    // for: 6
+    // for: 7
+    // for: 8                                            // for: 9
+    // for: 10
+    // for: 11
+    // end: 11
+
+}
