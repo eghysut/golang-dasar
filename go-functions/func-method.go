b58f21b93c4476b07228052a7a5ec1b6cfe99f64
eghysut <egiajjah15@gmail.com>
2023-08-24 20:11:03 +0700

    fix: Add func-method file

---
+++ go-functions/func-method.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// Fungsi Method(metode)
+
+type PersegiPanjang struct {
+    Panjang int
+    Lebar int
+}
+
+func (p PersegiPanjang) HitungLuas() int {
+    return p.Panjang * p.Lebar
+}
+
+func main() {
+
+    val := PersegiPanjang{Panjang: 2, Lebar: 3}
+    fmt.Println("Persegi panjang:", val)
+    // Output:
+    // Persegi panjang: {2 3}
+
+    luas := val.HitungLuas()
+    fmt.Println("luas:", luas)
+    // Output:
+    // luas: 6
+
+}
