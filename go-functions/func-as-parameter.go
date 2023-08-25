b2fb98afec6494cdebba0cb935a2a2a17a5a5493
eghysut <egiajjah15@gmail.com>
2023-08-25 22:57:36 +0700

    fix: Add func-as-parameter file

---
+++ go-functions/func-as-parameter.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+)
+
+func getText(nama string, role func(string) string) {
+    rvRole := role(nama)
+    fmt.Println("sebelum:", nama)
+    fmt.Println("sesudah:", rvRole)
+}
+
+// fungsi untuk huruf besar
+func setText(nama string) string {
+    return strings.ToUpper(nama) 
+}
+
+func main() {
+    getText("Alice", setText)
+    // Output:
+    // sebelum: Alice
+    // sesudah: ALICE
+}
