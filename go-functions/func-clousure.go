064c9abccf8088f30f5dd7646e2d61308a566cb0
eghysut <egiajjah15@gmail.com>
2023-08-26 16:17:50 +0700

    fix: Add func-clousure file

---
+++ go-functions/func-clousure.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// Fungsi Clousure
+
+func main() {
+    nama := "Hello"
+
+    // Closure: fungsi yang mengakses variabel x
+    fungsiClosure := func() {
+        fmt.Println(nama)
+    }
+
+    // Mengubah nama
+    nama = "Golang" 
+
+    // Memanggil closure
+    fungsiClosure() 
+    // Output: Golang
+
+    // contoh sederhana
+    jumlah := 1
+    next := func() {
+	fmt.Println(jumlah)
+    }
+
+    for jumlah <= 3 {
+	next()
+	jumlah++
+    }
+
+    fmt.Println("global:", jumlah)
+
+}
