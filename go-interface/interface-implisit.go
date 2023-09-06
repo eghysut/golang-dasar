5f6d3c10245af0a501507d03c7fda5b17f29a7f2
eghysut <egiajjah15@gmail.com>
2023-09-06 20:26:46 +0700

    fix: Add interface-implisit file

---
+++ go-interface/interface-implisit.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// Interface dipenuhi secara implisit
+// Sebuah tipe mengimplementasikan sebuah interface dengan mengimplementasikan method-methodnya. 
+// Tidak ada deklarasi eksplisit; tidak ada perintah "implements".
+
+// Interface implisit memisahkan definisi dari sebuah interface dari implementasinya, yang bisa saja muncul dalam paket manapun tanpa adanya penataan sebelumnya.
+
+type Kontrak interface{
+    Foo()
+}
+
+type Mahasiswa struct{
+    Nama string
+}
+
+
+// Method berikut berarti type Mahasiswa  mengimplementasikan interface Kontrak,
+// tapi kita tidak perlu secara eksplisit mendeklarasikannya.
+func (mhs Mahasiswa) Foo() {
+    fmt.Println(mhs.Nama)
+}
+
+func main() {
+    var x Kontrak = Mahasiswa{Nama: "Alice"}
+    x.Foo()
+    // Output: Alice
+
+    y := Mahasiswa{Nama: "Carl"}
+    y.Foo()
+    // Output: Carl
+
+}
