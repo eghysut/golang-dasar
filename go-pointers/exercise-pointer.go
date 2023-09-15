3972a7aa59cf22a7cfde9c037bf450aceb0bde64
eghysut <egiajjah15@gmail.com>
2023-09-15 10:34:13 +0700

    fix: Add exercise-pointer file

--- go-pointers/exercise-pointer.go
+++ go-pointers/exercise-pointer.go
@@ -2,11 +2,14 @@ package main
 
 import "fmt"
 
+
+// fungsi pass by value
 func PassByValue(angka int) {
     angka = angka * 2
     fmt.Println("PassByValue:", angka)
 }
 
+// fungsi pass by reference (pointer)
 func PassByReference(angka *int) {
     *angka = *angka * 2
     fmt.Println("PassByReference:", *angka)
@@ -14,13 +17,19 @@ func PassByReference(angka *int) {
 
 func main() {
     var i int = 100
-    fmt.Println(i)
-    // Output: 100
+    fmt.Println("Value i:", i)
+    // Output: Value i: 100
     
     PassByValue(i)
     fmt.Println("Value i:", i)
+    // Output:
+    // PassByValue: 200
+    // Value i: 100
 
     PassByReference(&i)
     fmt.Println("Value i:", i)
+    // Output:
+    // PassByReference: 200
+    // Value i: 200
 
 }
