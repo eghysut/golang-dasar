d47cb5f569bbde714d905ccfeba52322fa53e175
eghysut <egiajjah15@gmail.com>
2023-09-24 02:23:02 +0700

    fix: Add multiple-variabel file

---
+++ go-variables/multiple-variabel.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+
+func main() {
+    // Mendeklarasikan 2 variabel bertipe int.
+    var a, b int
+    a, b = 10, 20
+    fmt.Println(a, b)
+    // Output: 10 20
+
+    // Mendeklarasikan 3 variabel bertipe string
+    var x, y, z string = "Alice", "Carl", "Eliot"
+    fmt.Println(x, y, z)
+    // Output: Alice Carl Eliot
+
+
+}
