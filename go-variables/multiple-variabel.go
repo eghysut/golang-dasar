44a48b0fd76e142cbceff956b3bef7489a09c4ab
eghysut <egiajjah15@gmail.com>
2023-09-24 12:31:29 +0700

    fix: Add multiple-variable file

--- go-variables/multiple-variabel.go
+++ go-variables/multiple-variabel.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+// Deklarasi variabel dapat "difaktorkan" ke dalam blok
+var (
+    nama string = "Golang"
+    umur int = 15
+    
+)
 
 func main() {
     // Mendeklarasikan 2 variabel bertipe int.
@@ -15,5 +21,14 @@ func main() {
     fmt.Println(x, y, z)
     // Output: Alice Carl Eliot
 
+    fmt.Println(nama, umur)
+    // Output: Golang 15
+
+    var (
+        p int = 100
+        q int = 200
+    )
+    fmt.Println(p, q)
+    // Output: 100, 200
 
 }
