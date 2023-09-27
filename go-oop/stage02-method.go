8a7e61305ce27e1035c0bed14a7fca360f218e9d
eghysut <egiajjah15@gmail.com>
2023-09-27 12:21:48 +0700

    fix: Add stage02-method file

---
+++ go-oop/stage02-method.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+// Deklarasi struct Mahasiswa yang memiliki fields Nama dan Umur
+type Mahasiswa struct{
+    Nama string
+    Umur int
+}
+
+// Method dengan receiver Mahasiswa
+func (m Mahasiswa) display() {
+    fmt.Printf("nama: %s umur: %d\n", m.Nama, m.Umur)
+}
+
+
+func main() {
+
+    // Membuat instance objek Mahasiswa
+    alice := Mahasiswa{Nama: "Alice", Umur: 20}
+    // Memanggil method display() pada objek Mahasiswa
+    alice.display()
+    // Output:
+    // nama: Alice umur: 20
+
+    // Membuat instance objek secara eksplisit
+    var carl Mahasiswa = Mahasiswa{Nama: "carl", Umur: 22}
+    // Memanggil method display() pada objek Mahasiswa
+    carl.display()
+    // Output:
+    // nama: carl umur: 22
+
+}
